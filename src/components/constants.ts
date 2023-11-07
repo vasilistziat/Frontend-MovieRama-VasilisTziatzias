09e74f471d65345a045714f94a49033f6ce14bce
vasilistziat <vtziatzias@doctoranytime.gr>
2023-11-07 09:41:45 +0200

    HTTPS Images

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,4 +1,6 @@
-export const imageDefaultUrl = 'http://image.tmdb.org/t/p/w300_and_h450_bestv2';
+export const imageDefaultUrl =
+    'https://image.tmdb.org/t/p/w300_and_h450_bestv2';
 export const imageBackdropDefaultUrl =
-    'http://image.tmdb.org/t/p/w1920_and_h800_multi_faces';
-export const avatarDefaultUrl = 'http://image.tmdb.org/t/p/w150_and_h150_face/';
+    'https://image.tmdb.org/t/p/w1920_and_h800_multi_faces';
+export const avatarDefaultUrl =
+    'https://image.tmdb.org/t/p/w150_and_h150_face/';
